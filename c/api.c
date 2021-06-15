03ce9d8ae6211876e53fd43154d9a9bb4c59170f
Gui Xuan <a20137@163.com>
2021-06-15 23:35:10 +0800

    build libcjson.so and linked into libapi.so, now json can be processed by libapi.so

--- c/api.c
+++ c/api.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <cJSON/cJSON.h>
 int set() {
 	printf("int set() called\n");
+
+	char *char_json = "{\"habit\":\"lol\"}";
+	//从缓冲区中解析出JSON结构
+	cJSON *json = cJSON_Parse(char_json);
+ 	//将传入的JSON结构转化为字符串 并打印
+	char *buf = NULL;
+	printf("data:%s\n",buf = cJSON_Print(json));
+	
+	free(buf);
+	cJSON_Delete(json);
+
 	return 1;
 }
 
--- run.sh
+++ run.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/bash
-cd c
-gcc -shared -o libapi.so api.c
+
+# build libcjson.so
+cd c/cJSON
+mkdir build
+cd build
+cmake ..
+make
+
+# build libapi.so
+cd ../../
+mv cJSON/build/libcjson.so* ./
+gcc -shared -o libapi.so api.c -I. -L. -lcjson
 cp api.h ../
 
+# build libchttpd.so
 cd ..
-go build -buildmode=c-shared -o libchttpd.so gx.io/cgo/chttpd
+LD_LIBRARY_PATH=./c go build -buildmode=c-shared -o libchttpd.so gx.io/cgo/chttpd
 
+# build chttpd (executable)
 cd c
-gcc -o chttpd  main.c -L. -L.. -lpthread -lapi -lchttpd
+gcc -o chttpd  main.c -Wl,-rpath-link . -L. -L.. -lpthread -lapi -lchttpd
+# or: LD_LIBRARY_PATH=. gcc -o chttpd  main.c -L. -L.. -lpthread -lapi -lchttpd
 mv chttpd ../
 
+# run chttpd
 cd ..
 LD_LIBRARY_PATH=.:c ./chttpd
