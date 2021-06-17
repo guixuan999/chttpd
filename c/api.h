3c0e280568845de98a04b088b47e1516963adb13
Gui Xuan <a20137@163.com>
2021-06-18 00:01:54 +0800

    write a macro register_var, so user no need to use do_resgister_var anymore.

--- c/api.h
+++ c/api.h
@@ -1,3 +1,6 @@
+#define register_var(name, type, var) do_register_var(__FILE__, name, type, var)
 extern int set();
-void do_register_var(const char* src_filename, const char* var_name, const char* var_type, void* var);
 char* get_vars();
+
+// better use register_var instead of do_register_var
+void do_register_var(const char* src_filename, const char* var_name, const char* var_type, void* var);
--- c/main.c
+++ c/main.c
@@ -9,12 +9,12 @@ int main(int argc, char* argv[]) {
 	start_httpd(addr);
 	
 	int int_param = 100;
-	do_register_var(__FILE__, "int_param", "int", &int_param);
-	char * str_param = "guixuan";
-	do_register_var(__FILE__, "str_param", "string", str_param);
+	register_var("int_param", "int", &int_param);
+	char * str_param = "wang hongyan";
+	register_var("str_param", "string", str_param);
 
 	int bool_param = 1;
-	do_register_var(__FILE__, "bool_param", "bool", &bool_param);
+	register_var("bool_param", "bool", &bool_param);
 
 	while(1);
 	printf("Quit...\n");
