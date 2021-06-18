50ca4f46f3781cbf8daaa3681d260ba48da0ccac
Gui Xuan <a20137@163.com>
2021-06-18 21:53:10 +0800

    add float parameter testcase optimise run.sh

--- c/main.c
+++ c/main.c
@@ -8,11 +8,12 @@
 int int_param = 100;
 char* str_param = NULL;
 int bool_param = 1;
+float float_param = 123.39;
 
 void* watcher(void* data) {
 	while(1) {
-		printf("%d, %s, %d\n", int_param, str_param, bool_param);
-		sleep(1);
+		printf("%8d, %20s, %1d, %8f\n", int_param, str_param, bool_param, float_param);
+		sleep(2);
 	}
 }
 
@@ -27,6 +28,8 @@ int main(int argc, char* argv[]) {
 	register_var("str_param", "string", &str_param);
 
 	register_var("bool_param", "bool", &bool_param);
+
+	register_var("float_param", "float", &float_param);
 
 	pthread_t tid;
 	pthread_create(&tid, NULL, watcher, NULL);
--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+base_dir=$(pwd)
 
 # build libcjson.so
+cd $base_dir
 f1=c/libcjson.so
 f2=c/libcjson.so.1
 f3=c/libcjson.so.1.7.14
 if [ -f $f1 ] && [ -f $f2 ] && [ -f $f3 ]; then
-    cd c/cJSON
-    cd build
     echo "$f1 exists, skip cJSON building"
 else
     cd c/cJSON
@@ -17,21 +17,21 @@ else
 fi
 
 # build libapi.so
-cd ../../
+cd $base_dir/c
 mv cJSON/build/libcjson.so* ./
 gcc -shared -o libapi.so api.c -I. -L. -lcjson -fPIC
 cp api.h ../
 
 # build libchttpd.so
-cd ..
+cd $base_dir
 LD_LIBRARY_PATH=./c go build -buildmode=c-shared -o libchttpd.so gx.io/cgo/chttpd
 
 # build chttpd (executable)
-cd c
+cd $base_dir/c
 gcc -o chttpd  main.c -Wl,-rpath-link . -L. -L.. -lpthread -lapi -lchttpd
 # or: LD_LIBRARY_PATH=. gcc -o chttpd  main.c -L. -L.. -lpthread -lapi -lchttpd
 mv chttpd ../
 
 # run chttpd
-cd ..
+cd $base_dir
 LD_LIBRARY_PATH=.:c ./chttpd
